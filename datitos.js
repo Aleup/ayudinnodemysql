74adb5aed6626f6b2ccad71262d111153ab009f2
Aleup <alllantoy@gmail.com>
2020-07-03 19:14:21 -0500

    cors 3

--- datitos.js
+++ datitos.js
@@ -32,57 +32,84 @@ router.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user')
 })
 
-//mysql
+
 router.post('/mysql-insert', function (req, res) {
   console.log('user: '+req.body.user+ 'and text: ' + req.body.text);
   const mensaje = { msj_text: req.body.text, user: req.body.user, record_date: new Date() };
   var id;
-  global.db.query('INSERT INTO mensaje SET ?', mensaje, (err, queryres) => {
-    if(err) {
-      res.status(400);
-      res.send('mysql-insert: error: '+ err);
-    }
-    console.log('Last insert ID:', queryres.insertId);
-    res.send('mysql-insert: success with id: ' + queryres.insertId);
+
+  global.pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.query('INSERT INTO mensaje SET ?', mensaje, (err, queryres) => {
+      connection.release();
+      if(err) {
+        res.status(400);
+        res.send('mysql-insert: error: '+ err);
+      }
+      console.log('Last insert ID:', queryres.insertId);
+      res.send('mysql-insert: success with id: ' + queryres.insertId);
+    });
   });
 })
 
 router.get('/mysql-select', function (req, res) {
-  global.db.query('SELECT * FROM mensaje', (err, queryres) => {
-    if(err) {
-      res.status(400);
-      res.send('mysql-select: error: '+ err);
-    }
-    console.log('Data received from Db:' + queryres);
-    res.send(queryres);
+  
+  global.pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+
+    connection.query('SELECT * FROM mensaje', (err, queryres) => {
+      connection.release()
+      if(err) {
+        res.status(400);
+        res.send('mysql-select: error: '+ err);
+      }
+      console.log('Data received from Db:' + queryres);
+      res.send(queryres); 
+    });
+
   });
 })
 
 router.delete('/mysql-delete/:id', function (req, res) {
   console.log('Deleting rows with id: ' + req.params.id);
-  global.db.query('DELETE FROM mensaje WHERE id = ?', req.params.id, (err, queryres) => {
-    if(err) {
-      res.status(400);
-      res.send('mysql-delete: error: '+ err);
-    }
-    console.log(queryres);
-    console.log('Deleted ' + queryres.affectedRows + ' row(s)');
-    res.send('Deleted ' + queryres.affectedRows + ' row(s)');
+
+  global.pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+
+    connection.pool.query('DELETE FROM mensaje WHERE id = ?', req.params.id, (err, queryres) => {
+      connection.release();
+      if(err) {
+        res.status(400);
+        res.send('mysql-delete: error: '+ err);
+      }
+      console.log(queryres);
+      console.log('Deleted ' + queryres.affectedRows + ' row(s)');
+      res.send('Deleted ' + queryres.affectedRows + ' row(s)');
+    });
   });
 })
 
 router.put('/mysql-update/:id', function (req, res) {
   console.log('user: '+req.body.user+ ' and text: ' + req.body.text + ' and id: ' + req.body.id);
-  var id;
-  global.db.query('UPDATE mensaje SET msj_text = ?, user = ?, record_date = ? WHERE id = ?', 
-  [req.body.text, req.body.user, new Date(), req.params.id], (err, queryres) => {
-    if(err) {
-      res.status(400);
-      res.send('mysql-update: error: '+ err);
-    }
-    console.log(queryres);
-    console.log(`Changed ${queryres.changedRows} row(s)`);
-    res.send('Changed ' + queryres.changedRows + ' row(s)');
+
+  global.pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('connected as id ' + connection.threadId);
+
+    connection.query('UPDATE mensaje SET msj_text = ?, user = ?, record_date = ? WHERE id = ?', 
+      [req.body.text, req.body.user, new Date(), req.params.id], (err, queryres) => {
+        connection.release();
+        if(err) {
+          res.status(400);
+          res.send('mysql-update: error: '+ err);
+        }
+        console.log(queryres);
+        console.log(`Changed ${queryres.changedRows} row(s)`);
+        res.send('Changed ' + queryres.changedRows + ' row(s)');
+    });
   });
 })
 
--- server.js
+++ server.js
@@ -9,25 +9,18 @@ console.log("Se levanto");
 
 var datitos = require('./datitos');
 
-var db = mysql.createConnection ({
+var pool = mysql.createPool ({
   host: 'us-cdbr-east-02.cleardb.com',
   user: 'be32dc6bb669c2',
   password: '9ceccd13',
   database: 'heroku_ca5316af2198711'
 });
 
-db.connect((err) => {
-  if (err) {
-      throw err;
-  }
-  console.log('Connected to database');
-});
-global.db = db;
+global.pool = pool;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", 
-  "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Request-Headers");
   res.header("Access-Control-Allow-Methods", "*");
   next();
 });
